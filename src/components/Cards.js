3fc859374439cd5fc4ab398197f654e98ee4fef5
tioRomel <54945265+tioRomel@users.noreply.github.com>
2019-09-09 17:02:21 -0300

    Update.componente-v1

---
+++ src/components/Cards.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import exerciseImg from '../images/exercise.png'
+import circlesImg from '../images/circles.png'
+import './styles/Card.css'
+/* background: url('../../images/circles.png') no-repeat, linear-gradient(to right, #A74CF2, #617BFB); */
+class Card extends React.Component {
+    render(){
+        const {title, description, img, leftColor, rightColor} = this.props
+        return (
+            <div className="card mx-auto Fitness-Card"
+            style={{
+                backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+            }}>
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={exerciseImg} className="float-right"/>
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>{title}</h1>
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card
